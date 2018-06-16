ceed244b77f6a8fcf68ec5de6b2293d7558a2d3f
wandesky <wandesky@localhost.localdomain>
2018-06-16 12:27:12 +0300

    Run test to introduce myself to TDD

--- simple/app.py
+++ simple/app.py
@@ -1,6 +1,6 @@
 def hello_world():
-    return 'helo world'
-    # pass
+    return 'hello world'
+    #pass
 def create_num_list(length):
     return [x for x in range(length)]
-#     pass
+    #pass
--- simple/myTest.py
+++ simple/myTest.py
@@ -3,8 +3,9 @@ from app import *
 class MyFirstTests(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world(), 'hello world')
-    # def test_custom_num_list(self):
-    #     self.assertEqual(len(create_num_list(10)), 10)
+
+    def test_custom_num_list(self):
+         self.assertEqual(len(create_num_list(10)), 10)
 
 if __name__ == "__main__":
     unittest.main()
